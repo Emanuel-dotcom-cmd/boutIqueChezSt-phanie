ff940d588cb5fd8003472a0814f07995cd4efc47
Emanuel-dotcom-cmd <m.merveille.emanuel@gmail.com>
2026-06-19 16:41:07 -0400

    Change server port to 8000
    Set the server to run on port 8000 instead of using the environment variable.

--- server.py
+++ server.py
@@ -310,5 +310,4 @@ def cancel():
 
 # ✅ Lancer serveur
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000)
-    app.run(debug=True, use_reloader=False,host="0.0.0.0", port=port)
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=8000)
