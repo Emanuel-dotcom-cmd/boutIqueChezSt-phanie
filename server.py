40d507ed063dfd870ba5a859dbbb516c38e7e575
Emanuel-dotcom-cmd <m.merveille.emanuel@gmail.com>
2026-06-19 16:39:51 -0400

    Update server to use environment variable for port

--- server.py
+++ server.py
@@ -310,4 +310,5 @@ def cancel():
 
 # ✅ Lancer serveur
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, port=8000)
+    port = int(os.environ.get("PORT", 8000)
+    app.run(debug=True, use_reloader=False,host="0.0.0.0", port=port)
